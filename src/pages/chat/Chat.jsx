76702d837cd4142bbd2ae14c74310d3d6a797313
gracekim527 <lemonherb0323@naver.com>
2024-08-05 01:10:30 +0900

    :sparkles: Feat: 호야 채팅 기능 구현 (#13)

--- src/pages/chat/Chat.jsx
+++ src/pages/chat/Chat.jsx
@@ -1,10 +1,60 @@
 // Chat.jsx
 import Menubar from "../../components/common/Menubar";
+import Header from "../../components/common/Header";
+import instance from "../../api/instance";
+import { useState } from "react";
 
 export default function Chat() {
+  const [input, setInput] = useState("");
+  const [chatHistory, setChatHistory] = useState([]);
+
+  const handleInputChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    if (input.trim() === "") return;
+    setInput("");
+    setChatHistory((prevHistory) => [...prevHistory, { content: input }]);
+
+    try {
+      const accessToken = localStorage.getItem("accessToken");
+      const response = await instance.post(
+        `/api/v1/hoya/chat?prompt=${input}`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`
+          }
+        }
+      );
+      const { content } = response.data.data.choices[0].message;
+
+      setChatHistory((prevHistory) => [...prevHistory, { content }]);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
-    <>
+    <div>
+      <Header title="호야와 채팅하기" backLink="/" />
+      <div>
+        {chatHistory.map((chat, index) => (
+          <div key={index}>{chat.content}</div>
+        ))}
+      </div>
+      <form onSubmit={handleFormSubmit}>
+        <input
+          type="text"
+          value={input}
+          onChange={handleInputChange}
+          placeholder="메시지를 입력하세요."
+        />
+        <button type="submit">전송</button>
+      </form>
       <Menubar />
-    </>
+    </div>
   );
 }
